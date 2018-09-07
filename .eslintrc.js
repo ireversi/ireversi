609a2e302e2dc9fb11a9867811005f7637efb49d
hi85 <33993836+hi85gh@users.noreply.github.com>
2018-09-07 22:15:04 +0900

    Add packages for Git pre-commit hook (husky, lint-staged, eslint-config-airbnb-base)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,28 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/essential',
+    'airbnb-base',
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ['vue'],
   // add your custom rules here
-  rules: {}
-}
+  rules: {},
+  overrides: [
+    {
+      files: ['*.{js,vue}'],
+      rules: {
+        'import/no-unresolved': false,
+        'import/no-extraneous-dependencies': false,
+      },
+    },
+  ],
+};
