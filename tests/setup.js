ddae8f170d8e536dcf78520069743f26e7fe18d8
hi85 <33993836+hi85gh@users.noreply.github.com>
2018-09-22 21:33:51 +0900

    Update Jest global settings

--- tests/setup.js
+++ tests/setup.js
@@ -1,4 +1,14 @@
-const { Nuxt } = require('nuxt');
+const { Nuxt, Builder } = require('nuxt');
 const { config } = require('./nuxt.config.js');
 
-module.exports = () => new Nuxt(config);
+const nuxt = new Nuxt(config);
+
+module.exports = {
+  nuxt,
+  build: async () => {
+    await new Builder(nuxt).build();
+  },
+  teardown: () => {
+    nuxt.close();
+  },
+};
