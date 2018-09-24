b0f215c03d10ce9dfa0ef65aaab8f91d457699bf
m_mitsuhide <m.mitsuhide@amatelus.com>
2018-09-24 09:20:51 +0900

    fix: lint 'global-require'

--- mock/setup.js
+++ mock/setup.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const path = require('path');
+const template = require('./db.template.js');
 
 const dbFile = path.join(__dirname, 'db.json');
 
 if (!fs.existsSync(dbFile)) {
-  const template = require('./db.template.js');
   fs.writeFileSync(dbFile, JSON.stringify(template), 'utf8');
 }
