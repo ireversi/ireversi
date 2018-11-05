3b27a89916ab8a6d5206848c6cf78e56ba68c3c4
Shohei Kai <kai.entertainment0304@gmail.com>
2018-11-06 02:15:49 +0900

    fix: sorting

--- api/src/utils/restoreMongo.js
+++ api/src/utils/restoreMongo.js
@@ -7,11 +7,10 @@ const restore = true;
 module.exports = {
   async restoreMongo() {
     const mg = await JSON.parse(JSON.stringify(await BoardHistoryModel.find({}, propFilter)));
-    // console.log(mg);
 
     // Mongoのデータが日付順に格納されているとは限らないため並び替え
     // eslint-disable-next-line
-    await mg.sort((a, b) => (a._id.substr(8, 16) > b._id.substr(8, 16) ? 1 : -1));
+    await mg.sort((a, b) => (a._id > b._id ? 1 : -1));
 
     for (let i = 0; i < mg.length; i += 1) {
       const { x, y, userId } = mg[i].piece;
