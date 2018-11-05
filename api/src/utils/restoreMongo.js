5a5c0f225f9716ed070329b9784fc3f51e3fff8d
Shohei Kai <kai.entertainment0304@gmail.com>
2018-11-06 02:08:18 +0900

    first commit

--- api/src/utils/restoreMongo.js
+++ api/src/utils/restoreMongo.js
@@ -1,14 +1,17 @@
 const BoardHistoryModel = require('../models/v2/BoardHistoryModel.js');
 const PieceStore = require('../models/v2/PieceStore.js');
 
-const propFilter = '-_id -__v';
+const propFilter = '-__v';
 const restore = true;
 
 module.exports = {
   async restoreMongo() {
     const mg = await JSON.parse(JSON.stringify(await BoardHistoryModel.find({}, propFilter)));
+    // console.log(mg);
+
     // Mongoのデータが日付順に格納されているとは限らないため並び替え
-    mg.sort((a, b) => (a.date > b.date ? 1 : -1));
+    // eslint-disable-next-line
+    await mg.sort((a, b) => (a._id.substr(8, 16) > b._id.substr(8, 16) ? 1 : -1));
 
     for (let i = 0; i < mg.length; i += 1) {
       const { x, y, userId } = mg[i].piece;
