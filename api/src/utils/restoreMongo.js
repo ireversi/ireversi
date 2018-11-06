e8c2876e4add0b13f3c5a520dc4247dd39038d12
Shohei Kai <kai.entertainment0304@gmail.com>
2018-11-07 06:45:52 +0900

    fix: delete submodule

--- api/src/utils/restoreMongo.js
+++ api/src/utils/restoreMongo.js
@@ -1,14 +1,16 @@
 const BoardHistoryModel = require('../models/v2/BoardHistoryModel.js');
 const PieceStore = require('../models/v2/PieceStore.js');
 
-const propFilter = '-_id -__v';
+const propFilter = '-__v';
 const restore = true;
 
 module.exports = {
   async restoreMongo() {
     const mg = await JSON.parse(JSON.stringify(await BoardHistoryModel.find({}, propFilter)));
+
     // Mongoのデータが日付順に格納されているとは限らないため並び替え
-    mg.sort((a, b) => (a.date > b.date ? 1 : -1));
+    // eslint-disable-next-line
+    await mg.sort((a, b) => (a._id > b._id ? 1 : -1));
 
     for (let i = 0; i < mg.length; i += 1) {
       const { x, y, userId } = mg[i].piece;
--- api/tests/utils/sendMongo.test.js
+++ api/tests/utils/sendMongo.test.js
@@ -40,33 +40,42 @@ function searchIndex(jwtIds, jwtId) {
   return ans;
 }
 
-describe('piece', () => {
+describe('MongoDB', () => {
   beforeAll(prepareDB);
   afterEach(deleteAllDataFromDB);
 
   // MongoDBのデータを残して次のテストを行う
   describe('piece', () => {
-    // デバッグ検証、candidates残り確認
+    // さらに多くのコマでテスト
     it('cannot be put on the same place3', async () => {
       // Reset
       await chai.request(app).delete(`${basePath}`);
-      PieceStore.deletePieces();
-      storePlayHistory.deleteStandbySendMongo();
+      await PieceStore.deletePieces();
+      await storePlayHistory.deleteStandbySendMongo();
 
       // Mongoに送信開始
       sendMongo.startSendingMongo();
-
-      const jwtIds = genJwtArr(3);
+      const jwtIds = genJwtArr(16);
       // Given
-      const pieces = array2Pieces.array2Pieces(
+      // 下の配列の基の構成
+      // [
+      //   11:17, 10:16, 14:22, 15:23,  9:13,
+      //    7:18,  4:15,  7:19, 12:20,  4:12,
+      //     5:8,   4:6,   5:7,   6:9,  9:14,
+      //     1:1,   2:2,   1:3, 15:24, 13:21,
+      //       0  , 4:4,   5:5,  7:10,  8:11,
+      // ]
+
+      const pieces = await array2Pieces.array2Pieces(
         [
-          0, 0, 0,
-          `${jwtIds[0].decode}:1`, [`${jwtIds[1].decode}:2`, `${jwtIds[2].decode}:3`], `${jwtIds[0].decode}:4`,
-          0, 0, 0,
+          `${jwtIds[11].decode}:17`, `${jwtIds[10].decode}:16`, `${jwtIds[14].decode}:22`, `${jwtIds[15].decode}:23`, `${jwtIds[9].decode}:13`,
+          `${jwtIds[7].decode}:18`, `${jwtIds[4].decode}:15`, `${jwtIds[7].decode}:19`, `${jwtIds[12].decode}:20`, `${jwtIds[4].decode}:12`,
+          `${jwtIds[5].decode}:8`, `${jwtIds[4].decode}:6`, `${jwtIds[5].decode}:7`, `${jwtIds[6].decode}:9`, `${jwtIds[9].decode}:14`,
+          `${jwtIds[1].decode}:1`, `${jwtIds[2].decode}:2`, `${jwtIds[1].decode}:3`, `${jwtIds[15].decode}:24`, `${jwtIds[13].decode}:21`,
+          0, `${jwtIds[4].decode}:4`, `${jwtIds[5].decode}:5`, `${jwtIds[7].decode}:10`, `${jwtIds[8].decode}:11`,
         ],
       );
 
-      // When
       // MongoDBに送るだけの処理
       for (let i = 0; i < pieces.length; i += 1) {
         const index = searchIndex(jwtIds, pieces[i].piece.userId);
@@ -76,44 +85,45 @@ describe('piece', () => {
           .set('Authorization', jwtIds[index].jwtId)
           .send(pieces[i].piece);
       }
-      await sleep(2000); // 2000ミリ秒待機
+      await sleep(2700); // 3000ミリ秒待機
 
       // DBに送るのを一旦停止
       await sendMongo.stopSendingMongo();
+      await PieceStore.deletePieces(); // 配列を空に。
+      await storePlayHistory.deleteStandbySendMongo(); // Mongoに送る前の配列も空に。
 
       /*
       ここからサーバ・DBに再接続して、期待値を与えテストする
         */
 
-      // Reset
       await chai.request(app).delete(`${basePath}`);
-      PieceStore.deletePieces(); // 配列を空に。
-      storePlayHistory.deleteStandbySendMongo(); // Mongoに送る前の配列も空に。
 
+      // When
       // MongoDBから値を取得して、judgePieceして、Piecesに入っていく
       await restoreMongo.restoreMongo();
 
-      const resPieces = PieceStore.getPieces();
-      resPieces.shift(); // x:0, y:0, userId:1 を削除
+      const resPieces = await PieceStore.getPieces();
+      await resPieces.shift(); // x:0, y:0, userId:1 を削除
 
       // Piecesに復元した盤面と照合するための期待値(下部でmatchesDBに再度変換)
       // 復元するときにjudgePieceしているので、めくり終えたあとの盤面を再現
-      const matches = array2Matchers.array2Matchers(
+      const matches = await array2Matchers.array2Matchers(
         [
-          0, 0, 0,
-          `${jwtIds[0].decode}:1`, `${jwtIds[0].decode}:2`, `${jwtIds[0].decode}:3`,
-          0, 0, 0,
+          `${jwtIds[11].decode}:17`, `${jwtIds[10].decode}:16`, `${jwtIds[14].decode}:22`, `${jwtIds[15].decode}:23`, `${jwtIds[9].decode}:13`,
+          `${jwtIds[7].decode}:18`, `${jwtIds[7].decode}:15`, `${jwtIds[7].decode}:19`, `${jwtIds[15].decode}:20`, `${jwtIds[9].decode}:12`,
+          `${jwtIds[5].decode}:8`, `${jwtIds[7].decode}:6`, `${jwtIds[7].decode}:7`, `${jwtIds[15].decode}:9`, `${jwtIds[9].decode}:14`,
+          `${jwtIds[1].decode}:1`, `${jwtIds[4].decode}:2`, `${jwtIds[7].decode}:3`, `${jwtIds[15].decode}:24`, `${jwtIds[13].decode}:21`,
+          0, `${jwtIds[4].decode}:4`, `${jwtIds[5].decode}:5`, `${jwtIds[7].decode}:10`, `${jwtIds[8].decode}:11`,
         ],
       );
 
       // MongoDB確認のため、matchesからstatus: falseのオブジェクトを抜いた配列
       const matchesDB = matches.filter(m => m.status === true);
 
-      // When
       // 1ピースずつ確認
       for (let i = 0; i < matchesDB.length; i += 1) {
         // Then
-        expect(resPieces).toContainEqual(expect.objectContaining(matchesDB[i].piece));
+        expect(resPieces[i]).toEqual(matchesDB[i].piece);
       }
       expect(resPieces).toHaveLength(matchesDB.length); // x: 0, y: 0のデフォルト値を考慮
     });
