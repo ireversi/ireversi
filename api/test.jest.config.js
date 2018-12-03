b7f64e2a8572d60eee128e681e31bee426dfd2e6
hi85 <33993836+hi85gh@users.noreply.github.com>
2018-12-03 17:04:09 +0900

    test up

---
+++ api/test.jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  globalSetup: '<rootDir>/tests/testSetup.js',
+  globalTeardown: '<rootDir>/tests/testSetup.js',
+  // testEnvironment: '<rootDir>/tests/mongo-environment.js',
+};
